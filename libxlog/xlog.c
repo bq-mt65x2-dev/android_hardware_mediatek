892e1ecea2fa98c1bf4c25ad872527d046b36b86
R0rt1z2 <rogerortizleal@gmail.com>
2022-08-21 23:47:25 +0200

    hardware: mediatek: libxlog: Implement {set,get}_level
    F libc    : CANNOT LINK EXECUTABLE "/system/bin/xlog": cannot locate symbol "xlogf_set_level" referenced by "/system/bin/xlog"...

--- libxlog/xlog.c
+++ libxlog/xlog.c
@@ -1,4 +1,22 @@
 #include <cutils/log.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define XLOG_DEVICE "/proc/xlog/setfil"
+
+#define XLOG_NAME_MAX_LEN 64
+
+#define XLOG_SET_LEVEL       12
+#define XLOG_GET_LEVEL       13
+#define XLOG_SET_TAG_LEVEL   14
+
+#define XLOG_FILTER_DEFAULT_LEVEL 0x00223222
+
+struct xlog_entry {
+	char name[XLOG_NAME_MAX_LEN];
+	uint32_t level;
+};
 
 struct xlog_record {
 	const char *tag_str;
@@ -25,3 +43,46 @@ int __xlog_buf_printf(int bufid, const struct xlog_record *rec, ...)
 void dl_unregister_notify_function(void){return;}
 
 void dl_register_notify_function(int (*load_notify_function) (const char *name,uintptr_t address,uintptr_t size ),int (*unload_notify_function) (const char *name, uintptr_t address)){return;}
+
+int xlogf_set_level(uint32_t level)
+{
+    int ret = -1;
+    int fd = open(XLOG_DEVICE, O_RDONLY);
+    if (fd >= 0) {
+        if (ioctl(fd, XLOG_SET_LEVEL, level) == 0) {
+            ret = 0;
+        }
+        close(fd);
+    }
+    return ret;
+}
+
+uint32_t xlogf_get_level(void)
+{
+    uint32_t level = XLOG_FILTER_DEFAULT_LEVEL;
+    int fd = open(XLOG_DEVICE, O_RDONLY);
+    if (fd >= 0) {
+        ioctl(fd,XLOG_GET_LEVEL, &level);
+        close(fd);
+    }
+
+    return level;
+}
+
+int xlogf_tag_set_level(const char *name, uint32_t level)
+{
+    int ret = -1;
+    int fd = open(XLOG_DEVICE, O_RDONLY);
+    if (fd >= 0) {
+        struct xlog_entry ent;
+
+        memset(&ent, 0, sizeof(sizeof(struct xlog_entry)));
+        strlcpy(ent.name, name, XLOG_NAME_MAX_LEN);
+        ent.level = level;
+        if (ioctl(fd, XLOG_SET_TAG_LEVEL, &ent) == 0) {
+            ret = 0;
+        }
+        close(fd);
+    }
+    return ret;
+}
